ca5050df5cfed19169e11f93a49bcc2b9664e872
terry-lardner <terryjx@gmail.com>
2016-02-15 23:56:55 +0000

    minor changes to article css rules and editor.js

--- js/editor.js
+++ js/editor.js
@@ -5,11 +5,11 @@
 	$contentContainer = $('#contentContainer'),
 	$contentViewport = $('#contentContainer').find('#contentViewport'),
 	$inputContainer = $('#inputContainer'),
-	$articleTitle = $('#inputContainer').find('#article-title'),
-	$imageUrl = $('#inputContainer').find('#imageUrl'),
-	$txtContent = $('#inputContainer').find('#txtContent'),
-	$txtContentFeat = $('#inputContainer').find('#txtContentFeat'),
-	$featured = $('#inputContainer').find('#featured'),
+	$inputArticleTitle = $('#inputContainer').find('#article-title'),
+	$inputImageUrl = $('#inputContainer').find('#imageUrl'),
+	$inputContent = $('#inputContainer').find('#txtContent'),
+	$inputContentFeat = $('#inputContainer').find('#txtContentFeat'),
+	$inputfeatured = $('#inputContainer').find('#featured'),
 	$btnSave = $('#inputContainer').find('#btnSave'),
 	$btnCancel = $('#inputContainer').find('#btnCancel'),
 	$formMsg = $('#inputContainer').find('#formMsg');
@@ -44,6 +44,7 @@
 		 5: Featured checkbox checked without actual featured content 
 		 6: Article already exists 
 		 7: Underscore in Article ID
+		 8: 'Style' element attribute found
 	*/
 
 	function getArticleByTitle() {
@@ -54,17 +55,17 @@
 			success: function(data) {
 				if (data) {	  	
 					$contentViewport.html(convertToHtml(data.content));	
-					$articleTitle.val(data.title.replace(/_/g, ' '));
-					$txtContentFeat.val(data.contentFeat);
-					$featured.val(data.featured);
-					$imageUrl.val(data.imageUrl);	
+					$inputArticleTitle.val(data.title.replace(/_/g, ' '));
+					$inputContentFeat.val(data.contentFeat);
+					$inputfeatured.val(data.featured);
+					$inputImageUrl.val(data.imageUrl);	
 
 					if (data.featured) {
-						$featured.prop('checked', true);
+						$inputfeatured.prop('checked', true);
 					}				
 				}
 					
-				evalContentViewport();		
+				evalContentViewport();
 			},
 			error: function(err) {
 				errorMsg = JSON.stringify(err);
@@ -73,28 +74,27 @@
 		});
 	}
 
-	function checkArticleByTitle(formTitle, article) {
+	function checkArticleByTitle(formTitle, article, callbackSave, callbackUpdate, callbackHandleError) {
 		//check if title already exists
 		$.ajax({
 			type: 'GET',
 			url: `${serverUrl}/${article.title}`,
 			success: function(data) {
-
 				if (isNewArticle) {
 					if (data) {
 						errorMsg = `Sorry, an article for <a target="_blank" href="${url}?${article.title}"><em>${formTitle}</em></a> already exists.`;
-		    			processFormError(6, errorMsg);
+		    			callbackHandleError(6, errorMsg);
 					} else {
 						article.featuredDate = 0;
-						ajaxSaveArticle(article);
+						callbackSave(article);
 					}
 				} else {
-					ajaxUpdateArticle(article);
+					callbackUpdate(article);
 				}
 			},
 			error: function(err) {
 				errorMsg = JSON.stringify(err);
-		    	processFormError(3, errorMsg);
+		    	callbackHandleError(3, errorMsg);
 			}
 		});
 	}
@@ -112,41 +112,47 @@
 		}	
 	}
 
-	function processForm(o) {
+	function processForm() {
 		const illegalPrefix = '<';
-	  	let content = $txtContent.val();
+	  	let content = $inputContent.val(),
+	  	contentTrimmed = content.replace(/\s+/g, '').toLowerCase(),
+	  	contentFeat = $inputContentFeat.val(),
+	  	contentFeatTrimmed = contentFeat.replace(/\s+/g, '').toLowerCase();
 
-	  	errorMsg = '';
+	  	//Clear existing error messages.
+		errorMsg = '';
+		removeStatusMsg();	  	
 
-	  	//Numbers, letters and whitespace allowed.
-	  	let regex = /^[a-z0-9\s]*$/i;
 
-		for (let i=0; i<illegal.length; i++) {    
+	  	//Handle Error cases
+		for (let i=0; i<illegal.length; i++) {
 		    illegalItem = illegal[i];
 
-		    //Error cases
-		    if (!content.trim()) {
-		    	errorMsg = 'Content is empty.';
-		    	processFormError(1, errorMsg);			
-		     	break;
-		    } else if (content.indexOf(illegalPrefix + illegalItem) > -1) {
+		    if (content.indexOf(illegalPrefix + illegalItem) > -1 || contentFeat.indexOf(illegalPrefix + illegalItem) > -1) {
 		    	errorMsg = `&lt;${illegalItem}&gt; tags are not allowed.`;
 		    	processFormError(2, errorMsg);
 				break;
-		    } else if (!$articleTitle.val().trim()) {
-		    	errorMsg = 'Article ID is required.';
-		    	processFormError(4, errorMsg);
-				break;
-		    } else if ($featured.is(':checked') && !$txtContentFeat.val().trim()) {
-		    	errorMsg = 'Featured Content must be added if you want to feature this article';
-		    	processFormError(5, errorMsg);
-				break;
-		    } else if ($articleTitle.val().match(regex) == null){
-		    	errorMsg = 'Only letters and numbers are allowed in the Article ID';
-		    	processFormError(7, errorMsg);
-				break;
-		    }
+		    } 
 		}
+
+		// let regex = /^[a-z0-9\s]*$/i;
+
+		if (!content.trim()) {
+	    	errorMsg = 'Content is empty.';
+	    	processFormError(1, errorMsg);			
+	    } else if (!$inputArticleTitle.val().trim()) {
+	    	errorMsg = 'Article ID is required.';
+	    	processFormError(4, errorMsg);
+	    } else if ($inputfeatured.is(':checked') && !$inputContentFeat.val().trim()) {
+	    	errorMsg = 'Featured Content must be added if you want to feature this article';
+	    	processFormError(5, errorMsg);
+	    } else if ($inputArticleTitle.val().match(/^[a-z0-9\s]*$/i) == null){
+	    	errorMsg = 'Only letters and numbers are allowed in the Article ID';
+	    	processFormError(7, errorMsg);
+	    } else if (contentTrimmed.indexOf('style=') > -1 || contentFeatTrimmed.indexOf('style=') > -1) {
+	    	errorMsg = 'Style attributes are not permitted';
+	    	processFormError(8, errorMsg);
+		} 
 	  
 		if (!errorMsg) {
 		  	if (!networkAvailable) {
@@ -159,14 +165,14 @@
 
 	function saveArticle() {
 		let article = {
-			title: $articleTitle.val().replace(/\s/g, '_'),
-			content: $txtContent.val(),
-			contentFeat: $txtContentFeat.val(),
-			featured: $featured.is(':checked'),
-			imageUrl: $imageUrl.val()
+			title: $inputArticleTitle.val().replace(/\s+/g, '_'),
+			content: $inputContent.val(),
+			contentFeat: convertToHtml($inputContentFeat.val()),
+			featured: $inputfeatured.is(':checked'),
+			imageUrl: $inputImageUrl.val()
 		};
 
-		checkArticleByTitle($articleTitle.val(), article);		
+		checkArticleByTitle($inputArticleTitle.val(), article, ajaxSaveArticle, ajaxUpdateArticle, processFormError);		
 	}
 
 	function removeStatusMsg() {
@@ -174,9 +180,7 @@
 	}
 
 	function processFormError(errcode, msg) {
-		removeStatusMsg();
 		$formMsg.addClass('callout alert');
-		// $formMsg.html(`Err ${errcode}: ${msg}`);
 		$formMsg.html(msg);
 	}
 
@@ -190,7 +194,7 @@
 				// Override edit state of our page the article can be updated without having to reload the page.
 				articleId = data.title;
 				isNewArticle = 0;
-				console.log(`ADDED article : [${data.title}]`);
+				console.log(`CREATED article : [${data.title}]`);
 			},
 			error: function(err) {
 				errorMsg = JSON.stringify(err);
@@ -201,21 +205,21 @@
 
 	function ajaxUpdateArticle(article) {
 		$.ajax({
-				type: 'PUT',
-				url: `${serverUrl}/updateArticle/${articleId}`,
-				data: article,
-				success: function(data) {
-					hideElements([$btnTestData, $inputContainer]);
-					showElements([$contentViewport]);
+			type: 'PUT',
+			url: `${serverUrl}/updateArticle/${articleId}`,
+			data: article,
+			success: function(data) {
+				hideElements([$btnTestData, $inputContainer]);
+				showElements([$contentViewport]);
 
-					processhtml(data.content, convertToHtml);
-					console.log(`UPDATED article : [${data.title}]`);
-				},
-				error: function(err) {
-					errorMsg = JSON.stringify(err);
-		    		processFormError(3, errorMsg);
-				}
-			});
+				processhtml(data.content, convertToHtml);
+				console.log(`UPDATED article : [${data.title}]`);
+			},
+			error: function(err) {
+				errorMsg = JSON.stringify(err);
+	    		processFormError(3, errorMsg);
+			}
+		});
 	}	
 
 	function processhtml(html, callback) {  
@@ -228,19 +232,30 @@
 
 	//Convert What-You-See to html format
 	function convertToHtml(html) {
-		html = html.replace(/\n<image\>\n/g, '<div class="image">');
-		html = html.replace(/\n\<\/image\>\n/g, '</div>');
-		html = html.replace(/\<img src=\"/g, '<img src="img/wiki-img/');
-		html = html.replace(/\n/g, '<br>');
+		html = html.replace(/\n<image\>\n/g, '<div class="image">')
+			.replace(/\n\<\/image\>\n/g, '</div>')
+			.replace(/\<img src=\"/g, '<img src="img/wiki-img/')
+			.replace(/\<b\>/g, '<strong>')
+			.replace(/\<\/b\>/g, '</strong>')
+			.replace(/\<i\>/g, '<em>')
+			.replace(/\<\/i\>/g, '</em>')
+			.replace(/\n/g, '<br>');
+		console.log('[View mode] Converted to HTML.');
 		return html;
 	}
 
 	//Convert html format to What-You-See 
 	function convertToWYS(content) {
-		content = content.replace(/\<br\>/g, '\n');
-		content = content.replace(/\<div class="image"\>/g, '\n<image>\n');		
-		content = content.replace(/\<\/div\>/g, '\n</image>\n');
-		content = content.replace(/\<img src=\"img\/wiki-img\//g, '<img src="');
+		content = content.replace(/\<br\>/g, '\n')
+				.replace(/\<div class="image"\>/g, '\n<image>\n')		
+				.replace(/\<\/div\>/g, '\n</image>\n')
+				.replace(/\<img src=\"img\/wiki-img\//g, '<img src="')
+				.replace(/\<strong\>/g, '<b>')
+				.replace(/\<\/strong\>/g, '</b>')
+				.replace(/\<em\>/g, '<i>')
+				.replace(/\<\/em\>/g, '</i>');
+
+		console.log('[Edit mode] Converted to What-You-See.');
 		return content;
 	}
 
@@ -262,6 +277,25 @@
 		} else {
 			arr[0].show();
 		}
+	}
+
+	function init() {
+		if (networkAvailable) {
+			$networkStatus.addClass('networkStatusOK');
+			$networkStatus.html('*ONLINE MODE*');
+
+			//If we have a query string, attempt to find the related article
+			if (queryIndex) {					
+				getArticleByTitle();			
+			}
+
+		} else {
+			$networkStatus.addClass('networkStatusNOK');
+			$networkStatus.html('*OFFLINE MODE*');
+		}
+
+		//Prevent initial flash of the entire input form before js has a chance to evaluate
+		$inputContainer.removeClass('hide');
 	}
 
 	/* =============================
@@ -296,27 +330,25 @@
 
 		let content = $contentViewport.html();
 
-		// hideElements([$contentViewport, $btnSave]);
 		hideElements([$contentViewport]);
 		showElements([$btnTestData, $inputContainer]);
 
-		if (!$contentViewport.html()) {
+		if (!content) {
 			//Assuming a new article
-			// showElements([$btnSave]);
-			$txtContent.val('');
-			$articleTitle.val('');
-			$imageUrl.val('');
-			$txtContent.val('');
-			$txtContentFeat.val('');
-			$featured.prop('checked', false);
+			$inputContent.val('');
+			$inputArticleTitle.val('');
+			$inputImageUrl.val('');
+			$inputContent.val('');
+			$inputContentFeat.val('');
+			$inputfeatured.prop('checked', false);
 
 			isNewArticle = 1;
 		} else {
 			//Assuming updating an existing article
-			$articleTitle.prop('disabled', true);
+			$inputArticleTitle.prop('disabled', true);
 			isNewArticle = 0;
 		}
-		$txtContent.val(convertToWYS(content));
+		$inputContent.val(convertToWYS(content));
 	});
 
 	//Cancel edit mode
@@ -329,70 +361,26 @@
 
 	//Input test data
 	$btnTestData.on('click', () => {
-		$articleTitle.val('Arthur Sifton');
-		$imageUrl.val('Arthur_Lewis_Watkins_Sifton.jpg');
+		$inputArticleTitle.val('Arthur Sifton');
+		$inputImageUrl.val('Arthur_Lewis_Watkins_Sifton.jpg');
 
 		let clientContent = new XMLHttpRequest();
 		clientContent.open('GET', './testArticle.html');
 		clientContent.onreadystatechange = function() {
-		  $txtContent.val(clientContent.responseText);
+		  $inputContent.val(clientContent.responseText);
 		}
 		clientContent.send();
 
 		let clientContentFeat = new XMLHttpRequest();
 		clientContentFeat.open('GET', './testFeature.html');
 		clientContentFeat.onreadystatechange = function() {
-		  $txtContentFeat.val(clientContentFeat.responseText);
+		  $inputContentFeat.val(clientContentFeat.responseText);
 		}
 		clientContentFeat.send();
 	});
 
 	//Save Article
 	$btnSave.on('click', processForm);
-	
-	function init() {
-		if (networkAvailable) {
-			$networkStatus.addClass('networkStatusOK');
-			$networkStatus.html('*ONLINE MODE*');
-
-			//If we have a query string, attempt to find the related article
-			if (queryIndex) {					
-				getArticleByTitle();			
-			}
-
-		} else {
-			// ***FOR OFFLINE MODE ONLY***
-				$articleTitle.val('Arthur Sifton');
-				$imageUrl.val('Arthur_Lewis_Watkins_Sifton.jpg');
-				$featured.prop('checked', true);
-
-				let clientContent = new XMLHttpRequest();
-				clientContent.open('GET', './testArticle.html');
-				clientContent.onreadystatechange = function() {
-
-					$contentViewport.html(convertToHtml(clientContent.responseText));
-					evalContentViewport();
-				}
-				clientContent.send();
-
-				let clientContentFeat = new XMLHttpRequest();
-				clientContentFeat.open('GET', './testFeature.html');
-				clientContentFeat.onreadystatechange = function() {
-					$txtContentFeat.val(clientContentFeat.responseText);
-				}
-				clientContentFeat.send();
-			// ***FOR OFFLINE MODE ONLY***
-
-			$networkStatus.addClass('networkStatusNOK');
-			$networkStatus.html('*OFFLINE MODE*');
-		}
-		
-		//Evaluate whether to show edit form or not
-		// evalContentViewport();
-
-		//Prevent initial flash of the entire input form before js has a chance to evaluate
-		$inputContainer.removeClass('hide');
-	}
 
 	/* 
 		Init
