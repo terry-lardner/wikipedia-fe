bf05f016ee1b6d970f9056ea01455dab7baf73d1
terry-lardner <terryjx@gmail.com>
2016-03-01 16:58:09 +0000

    fixed test data and improved search feature

--- js/search.js
+++ js/search.js
@@ -12,14 +12,15 @@
 	const url = window.location.href;
 	const queryIndex = url.indexOf('?') + 1;
 	const q = url.substring(queryIndex);
-
+	
 	//Do 'q.substring(2).trim()' if we don't want to send empty search strings to server.
 	if (queryIndex && templateSearch) {
 		$.ajax({
 			type: 'GET',
 			url: `${serverUrl}/search/${q}`,
 			success: function(data) {
-				$searchResultsTitle.html(`Found ${data.length} results for '${q.substring(2)}' :`);
+				let userQuery = q.substring(2).replace(/\%22/g, '').replace(/\+/g, ' ');
+				$searchResultsTitle.html(`Found ${data.length} results for '${userQuery}' :`);
 				
 				$.each(data, function(i, result) {
 					result.title_mod = result.title.replace(/_/g, ' ');
@@ -27,7 +28,7 @@
 				});
 			},
 			error: function(err) {
-				console.log("Error getting search results [" + JSON.stringify(err) + "]");
+				console.log('Error getting search results [' + JSON.stringify(err) + ']');
 			}
 		});
 	}	
